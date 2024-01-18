4c2702e52d7d0191b0aa4b0b46f440a8dc62996a
lija12-3 <105269384+lija12-3@users.noreply.github.com>
2024-01-18 23:36:00 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-sudo apt update -y
+ apt update -y
 
  apt install fontconfig openjdk-17-jre
 
 java -version
 
-sudo apt update -y
+ apt update -y
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+ wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-sudo apt update -y
+ apt update -y
 
-sudo add-apt-repository universe -y
 
-sudo apt-get install jenkins -y
 
-sudo service jenkins start
+ apt-get install jenkins -y
+
+ service jenkins start
 
 cat /var/lib/jenkins/secrets/initialAdminPassword
 
