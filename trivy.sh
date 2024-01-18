6d91bf6458b96d64f2fb10aca582717d60e86cbb
lija12-3 <105269384+lija12-3@users.noreply.github.com>
2024-01-18 23:54:05 +0530

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -1,5 +1,7 @@
-sudo apt-get install wget apt-transport-https gnupg lsb-release
+
+ apt-get install wget apt-transport-https gnupg lsb-release
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt-get update
-sudo apt-get install trivy
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.li>
+ apt-get update
+ apt-get install trivy
+
